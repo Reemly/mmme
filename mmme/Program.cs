c625079ccc4367ae8bc57e5cfa8205fdb5ea5d1d
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-09-21 11:08:47 +0200

    dialogue

--- mmme/Program.cs
+++ mmme/Program.cs
@@ -1,22 +1,77 @@
 ﻿using System;
+using System.Threading;
 
-string name = "22";
-
-int points = 0;
-
-points = points +1;
-
-points += 3;
-
-points++;
+System.Console.WriteLine("Hello and welcome to, 50 cents richer!!");
 
 
+System.Console.WriteLine($"First question! How many legs does a horse have? A) 1534 B) 4 C) 3 or D) 5");
 
 
-int hp;
-int xp;
+string Answer = Console.ReadLine();
+Answer = Answer.ToLower();
 
-System.Console.WriteLine(name + name);
-System.Console.WriteLine(points);
+
+if (Answer == "a")
+{
+    System.Console.WriteLine("lol.");
+    Console.ReadLine();
+}
+
+
+
+if (Answer == "b")
+{
+    System.Console.WriteLine("You got it, nice job.");
+    Console.ReadLine();
+}
+
+if (Answer == "c")
+{
+    System.Console.WriteLine("bruh.");
+    Console.ReadLine();
+
+}
+
+
+if (Answer == "d")
+{
+    System.Console.WriteLine("oh?");
+    Console.ReadLine();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int points = 0;
+
+
+
+
+
+
 
 Console.ReadLine();
+
