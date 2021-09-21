e8b236589249d8e6b034a69f5138320a7f6f73ec
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-09-21 10:25:50 +0200

    iapologizeoriginalgangstar

---
+++ mmme/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+string name = "22";
+
+int points = 0;
+
+points = points +1;
+
+points += 3;
+
+points++;
+
+
+
+
+int hp;
+int xp;
+
+System.Console.WriteLine(name + name);
+System.Console.WriteLine(points);
+
+Console.ReadLine();
